e44a521ef305706fced5ddbde293e0bd64d89dad
윤예리 <yoonr721@gmail.com>
2024-11-06 17:57:38 +0900

    1106

---
+++ JavaStudy/16933/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m, k;
+    int[][] arr;
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+            }
+        }
+    }
+}
