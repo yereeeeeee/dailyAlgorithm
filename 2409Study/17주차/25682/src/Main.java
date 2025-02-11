f21ff142a2a898c015e491798ee2f45fb1191a59
윤예리 <yoonr721@gmail.com>
2025-02-11 17:59:27 +0900

    11

--- 2409Study/17주차/25682/src/Main.java
+++ 2409Study/17주차/25682/src/Main.java
@@ -2,7 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n, m, k;
+    static char[][] board;
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            char[] tmp = br.readLine().toCharArray();
+
+            for (int j = 0; j < m; j++) {
+                board[i][j] = tmp[j];
+            }
+        }
+
+
+
     }
 }
