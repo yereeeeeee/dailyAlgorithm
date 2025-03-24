01290098165112f4c2f37074dd7fa2a5513dac49
윤예리 <yoonr721@gmail.com>
2025-03-24 09:14:06 +0900

    0324

---
+++ 12기 실습코치/B6198.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class B6198 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ 12기 실습코치/S12/src/UserSolution.java
@@ -0,0 +1,16 @@
+class UserSolution
+{
+    public void init(int N, int mMap[][])
+    {
+    }
+
+    public int numberOfCandidate(int M, int mStructure[])
+    {
+        return 0;
+    }
+
+    public int maxArea(int M, int mStructure[], int mSeaLevel)
+    {
+        return 0;
+    }
+}
