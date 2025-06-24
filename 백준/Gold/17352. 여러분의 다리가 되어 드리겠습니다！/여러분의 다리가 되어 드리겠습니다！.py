504eac9c60c4a0d33891ed509caa6310c0da066b
yereeeeeee <156388511+yereeeeeee@users.noreply.github.com>
2025-06-24 12:12:43 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 352 ms, Memory: 114636 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -6,24 +6,23 @@ par = [i for i in range(n+1)]
 
 def find(x):
     if x != par[x]:
-        par[x] = find(par[x])  # 경로 압축
+        par[x] = find(par[x])
+    
     return par[x]
 
 def union(a, b):
     a = find(a)
     b = find(b)
-    
-    if a != b:
-        if a < b:
-            par[b] = a
-        else:
-            par[a] = b
+
+    if a < b:
+        par[b] = a
+    else:
+        par[a] = b
 
 for _ in range(n-2):
     a, b = map(int, input().split())
     union(a, b)
 
-# 각 노드의 실제 루트를 찾아서 저장
 roots = set()
 for i in range(1, n+1):
     roots.add(find(i))
